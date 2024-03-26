a67f1e95921375573ef4c891bae836a97fdda332
Preston Lo <prestonloz97@gmail.com>
2024-03-26 15:57:32 +0000

    WIP

---
+++ bashdb_dir/makefiledb.mk
@@ -0,0 +1,6 @@
+
+# make -f /workspace/vscode-bash-debug/bashdb_dir/makefiledb.mk input_file=abc
+FILE=$(input_file)
+
+a:
+	make -f $(FILE)
--- src/spawnBash.ts
+++ src/spawnBash.ts
@@ -27,6 +27,6 @@ export function spawnBashScript(scriptCode: string, pathBash: string, outputHand
 export function spawnBashScriptSync(scriptCode: string, pathBash: string, spawnTimeout: number): SpawnSyncReturns<Buffer> {
     const currentShell = (process.platform === "win32") ? getWSLLauncherPath(false) : pathBash;
     const optionalBashPathArgument = (currentShell !== pathBash) ? pathBash : "";
-
-    return spawnSync(currentShell, [optionalBashPathArgument, "-c", scriptCode].filter(arg => arg !== ""), { timeout: spawnTimeout, shell: false });
+    // here fix the error invalid arguement `-c` is given
+    return spawnSync(currentShell, [optionalBashPathArgument, "-f", scriptCode].filter(arg => arg !== ""), { timeout: spawnTimeout, shell: false });
 }
